14a663f4c814ab625bd363e11b403fb158adce5c
Yury Kaliada <fut.wrk@gmail.com>
2013-11-21 21:41:08 +0300

    fixed query parsing
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- lib/codenjoy_ruby_client.rb
+++ lib/codenjoy_ruby_client.rb
@@ -4,7 +4,7 @@ class CodenjoyRubyClient
   def self.run(host='0.0.0.0', port=8888, &answer)
     codenjoy_server = lambda do |env|
       decoded_query = URI.decode_www_form env['QUERY_STRING']
-      query = Hash.new[decoded_query]
+      query = Hash[decoded_query]
 
       [200, {"Content-Type" => "text/plain"}, [answer.call(query)]]
     end
